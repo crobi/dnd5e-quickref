75c9d8a0f8b53da48e1145e7612c952435adb3ae
crobi <robin@dbservice.com>
2015-01-21 16:01:42 +0100

    modal

--- js/quickref.js
+++ js/quickref.js
@@ -1,33 +1,67 @@
-function add_quickref_item(parent, title, icon, subtitle) {
-
+function add_quickref_item(parent, data, type) {
+    var icon = data.icon || "perspective-dice-six-faces-one";
+    var subtitle = data.subtitle || "";
+    var title = data.title || "[no title]";
 
     var item = document.createElement("div");
     item.className += "item itemsize"
     item.innerHTML =
     '\
-    <div class="item-icon iconsize icon-'+icon+'"></div>\
+    <div class="item-icon iconsize icon-' + icon + '"></div>\
     <div class="item-text-container text">\
         <span class="item-title">' + title + '</span><br />\
         <span class="item-desc">' + subtitle + '</span>\
     </div>\
     ';
 
-    parent.appendChild(item);
+    var style = window.getComputedStyle(parent.parentNode.parentNode);
+    var color = style.backgroundColor;
 
+    item.onclick = function () {
+        show_modal(data, color, type);
+    }
+
+    parent.appendChild(item);
 }
 
-function fill_section(data, parentname) {
+function show_modal(data, color, type) {
+    var title = data.title || "[no title]";
+    var subtitle = data.description || data.subtitle || "";
+    var bullets = data.bullets || [];
+    type = type || "";
+    color = color || "black"
+
+    $("body").addClass("modal-open");
+    $("#modal").addClass("modal-visible");
+    $("#modal-backdrop").css("height", screen.height + "px");
+    $("#modal-container").css("background-color", color).css("border-color", color);
+    $("#modal-title").text(title).append("<span class=\"float-right\">" + type + "</span>");
+    $("#modal-subtitle").text(subtitle);
+
+    var bullets_html = bullets.map(function (item) { return "<li>" + item + "</li>"; }).join("\n");
+    $("#modal-bullets").html(bullets_html);
+}
+
+function hide_modal() {
+    $("body").removeClass("modal-open");
+    $("#modal").removeClass("modal-visible");
+}
+
+function fill_section(data, parentname, type) {
     var parent = document.getElementById(parentname);
     data.forEach(function (item) {
-        add_quickref_item(parent, item.title, item.icon, item.subtitle);
+        add_quickref_item(parent, item, type);
     });
 }
 
-function fill_items() {
-    fill_section(data_movement, "basic-movement");
-    fill_section(data_action, "basic-actions");
-    fill_section(data_bonusaction, "basic-bonus-actions");
-    fill_section(data_reaction, "basic-reactions");
+function init() {
+    fill_section(data_movement, "basic-movement", "Move");
+    fill_section(data_action, "basic-actions", "Action");
+    fill_section(data_bonusaction, "basic-bonus-actions", "Bonus action");
+    fill_section(data_reaction, "basic-reactions", "Reaction");
+
+    var modal = document.getElementById("modal");
+    modal.onclick = hide_modal;
 }
 
-fill_items();
+$(window).load(init);
