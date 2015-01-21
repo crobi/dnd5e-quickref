0be6e092e9c381c48b6c14f763e092e4e386cba8
crobi <robin@dbservice.com>
2015-01-21 22:25:41 +0100

    Preview

---
+++ preview/js/data_movement.js
@@ -0,0 +1,113 @@
+data_movement = [
+    {
+        title: "Move",
+        icon: "run",
+        subtitle: "Cost: 5ft per 5ft",
+        description: "Movement cost: 5ft per 5ft moved",
+        bullets: [
+            "If you have more than one speed, such as your walking speed and a flying speed, you can switch back and forth between your speeds during your move. Whenever you switch, subtract the distance you've already moved from the new speed.",
+            "You can move through a nonhostile creature's space.",
+            "You can move through a hostile creature's space only if the creature is at least two sizes larger or smaller than you.",
+            "Another creature's space is difficult terrain for you.",
+            "Whether a creature is a friend or an enemy, you can't willingly end your move in its space."
+        ]
+    },
+    {
+        title: "Climb",
+        icon: "crags",
+        subtitle: "Cost: 10ft per 5ft",
+        description: "Movement cost: 10ft per 5ft climbed",
+        bullets: [
+            "May involve a Strength (Athletics) check if the climb is difficult"
+        ]
+    },
+    {
+        title: "Swim",
+        icon: "at-sea",
+        subtitle: "Cost: 10ft per 5ft",
+        description: "Movement cost: 10ft per 5ft swum",
+        bullets: [
+            "May involve a Strength (Athletics) check if the swim is difficult"
+        ]
+    },
+    {
+        title: "Drop prone",
+        icon: "falling",
+        subtitle: "Cost: 0ft",
+        description: "Movement cost: 0ft (free)",
+        bullets: [
+            "You can drop prone without using any of your speed",
+            "To move while prone, you must crawl or use magic such as teleportation",
+            "Dropping prone adds the <i>Prone</i> condition (melee attacks against you have advantage, ranged attacks against you have disadvantage, your own attacks have disadvantage)"
+        ]
+    },
+    {
+        title: "Crawl",
+        icon: "crawl",
+        subtitle: "Cost: 10ft per 5ft",
+        description: "Movement cost: 10ft per 5ft crawled",
+        bullets: [
+
+        ]
+    },
+    {
+        title: "Stand up",
+        icon: "strong",
+        subtitle: "Cost: half movement speed",
+        description: "Movement cost: half of your speed",
+        bullets: [
+            "You can't stand up if you don't have enough movement left or if your speed is 0"
+        ]
+    },
+    {
+        title: "High jump",
+        icon: "wingfoot",
+        subtitle: "Cost: 5ft per 5ft",
+        description: "Movement cost: 5ft per 5ft jumped",
+        bullets: [
+            "You leap into the air a number of feet equal to <b>3 + your Strength modifier</b> if you move at least 10 feet on foot immediately before the jump.",
+            "When you make a standing high jump, you can jump only half that distance.",
+            "You can extend your arms half your height above yourself during the jump.",
+            "In some circumstances, your DM might allow you to make a Strength (Athletics) check to jump higher than you normally can."
+        ]
+    },
+    {
+        title: "Long jump",
+        icon: "wingfoot",
+        subtitle: "Cost: 5ft per 5ft",
+        description: "Movement cost: 5ft per 5ft jumped",
+        bullets: [
+            "You cover a number of feet up to your <b>Strength score</b> if you move at least 10 feet on foot immediately before the jump.",
+            "When you make a standing long jump, you can leap only half that distance",
+            "May involve a DC 10 Strength (Athletics) check to clear a low obstacle (no taller than a quarter of the jump's distance). You hit the obstacle on a failed check.",
+            "May involve a DC 10 Dexterity (Acrobatics) check to land on your feet in difficult terrain. You land prone on a failed check."
+        ]
+    },
+    {
+        title: "Improvise",
+        icon: "juggler",
+        subtitle: "Any stunt not on this list",
+        description: "Perform any movement or stunt you can imagine",
+        bullets: [
+            "When you describe a kind of movement not detailed elsewhere in the rules, the DM tells you whether it is possible and what kind of roll you need to make, if any, to determine success or failure."
+        ]
+    },
+    {
+        title: "Difficult terrain",
+        icon: "stone-pile",
+        subtitle: "Cost modifier: +5ft per 5ft",
+        description: "Moving in difficult terrain costs an additional 5ft per 5ft of movement",
+        bullets: [
+        ]
+    },
+    {
+        title: "Grapple move",
+        icon: "grab",
+        subtitle: "Modifier: speed halved",
+        description: "Drag or carry the grappled creature with you",
+        bullets: [
+            "If you move while grappling another creature, your speed is halved, unless the creature is two or more sizes smaller than you.",
+            "See the attack action for how to grapple a creature."
+        ]
+    }
+]
---
+++ preview/js/quickref.js
@@ -0,0 +1,67 @@
+function add_quickref_item(parent, data, type) {
+    var icon = data.icon || "perspective-dice-six-faces-one";
+    var subtitle = data.subtitle || "";
+    var title = data.title || "[no title]";
+
+    var item = document.createElement("div");
+    item.className += "item itemsize"
+    item.innerHTML =
+    '\
+    <div class="item-icon iconsize icon-' + icon + '"></div>\
+    <div class="item-text-container text">\
+        <span class="item-title">' + title + '</span><br />\
+        <span class="item-desc">' + subtitle + '</span>\
+    </div>\
+    ';
+
+    var style = window.getComputedStyle(parent.parentNode.parentNode);
+    var color = style.backgroundColor;
+
+    item.onclick = function () {
+        show_modal(data, color, type);
+    }
+
+    parent.appendChild(item);
+}
+
+function show_modal(data, color, type) {
+    var title = data.title || "[no title]";
+    var subtitle = data.description || data.subtitle || "";
+    var bullets = data.bullets || [];
+    type = type || "";
+    color = color || "black"
+
+    $("body").addClass("modal-open");
+    $("#modal").addClass("modal-visible");
+    $("#modal-backdrop").css("height", window.innerHeight + "px");
+    $("#modal-container").css("background-color", color).css("border-color", color);
+    $("#modal-title").text(title).append("<span class=\"float-right\">" + type + "</span>");
+    $("#modal-subtitle").text(subtitle);
+
+    var bullets_html = bullets.map(function (item) { return "<li>" + item + "</li>"; }).join("\n");
+    $("#modal-bullets").html(bullets_html);
+}
+
+function hide_modal() {
+    $("body").removeClass("modal-open");
+    $("#modal").removeClass("modal-visible");
+}
+
+function fill_section(data, parentname, type) {
+    var parent = document.getElementById(parentname);
+    data.forEach(function (item) {
+        add_quickref_item(parent, item, type);
+    });
+}
+
+function init() {
+    fill_section(data_movement, "basic-movement", "Move");
+    fill_section(data_action, "basic-actions", "Action");
+    fill_section(data_bonusaction, "basic-bonus-actions", "Bonus action");
+    fill_section(data_reaction, "basic-reactions", "Reaction");
+
+    var modal = document.getElementById("modal");
+    modal.onclick = hide_modal;
+}
+
+$(window).load(init);
