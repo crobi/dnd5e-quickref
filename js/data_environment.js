7889410c0caafe801f0bde28aa202a37a1a48a70
SecretOnline <stu.thom.96@gmail.com>
2015-01-23 14:08:43 +1300

    Add environmental data
    Choosing the icons was the most difficult bit. If anyone else has a
    better idea for the icons, go ahead and change them.

--- js/data_environment.js
+++ js/data_environment.js
@@ -1,2 +1,118 @@
 data_environment = [
+    {
+        title: "Light obscurity",
+        icon: "bleeding-eye",
+        subtitle: "Dim light, patchy fog, moderate foliage.",
+        description: "",
+        reference: "PHB, pg. 183.",
+        bullets: [
+            "Creatures have disadvantage on Wisdom (Perception) checks that rely on sight."
+        ]
+    },
+    {
+        title: "Heavy obscurity",
+        icon: "lightning-tear",
+        subtitle: "Darkness, opaque fog, dense foliage.",
+        description: "",
+        reference: "PHB, pg. 183.",
+        bullets: [
+            "A creature in a heavily obscured area effectively suffers from the blinded condition."
+        ]
+    },
+    {
+        title: "Darkness",
+        icon: "worried-eyes",
+        subtitle: "Heavily obscured.",
+        description: "Darkness creates a heavily obscured area.",
+        reference: "PHB, pg. 183.",
+        bullets: [
+            "Characters face darkness outdoors at night (even most moonlit nights), within the confines of an unlit dungeon or a subterranean vault, or in an area of magical darkness."
+        ]
+    },
+    {
+        title: "Dim light",
+        icon: "semi-closed-eye",
+        subtitle: "Lightly obscured.",
+        description: "Dim light, also called shadows, creates a lightly obscured area.",
+        reference: "PHB, pg. 183.",
+        bullets: [
+            "An area of dim light is usually a boundary between a source of bright light, such as a torch, and surrounding darkness.",
+            "The soft light of twilight and dawn also counts as dim light. A particularly brilliant full moon might bathe the land in dim light."
+        ]
+    },
+    {
+        title: "Bright light",
+        icon: "star-pupil",
+        subtitle: "Normal vision.",
+        description: "Bright light lets most creatures see normally.",
+        reference: "PHB, pg. 183.",
+        bullets: [
+            "Gloomy days still provide bright light, as do torches, lanterns, fires, and other sources of illumination within a specific radius."
+        ]
+    },
+    {
+        title: "Blindsight",
+        icon: "one-eyed",
+        subtitle: "Perceive without sight.",
+        description: "Perceive your surroundings without relying on sight, within a certain radius.",
+        reference: "PHB, pg. 183.",
+        bullets: [
+            "Creatures without eyes, such as oozes, and creatures with echolocation or heightened senses, such as bats and true dragons, have this sense."
+        ]
+    },
+    {
+        title: "Darkvision",
+        icon: "semi-closed-eye",
+        subtitle: "Limited vision in darkness.",
+        description: "A creature with blindsight can perceive its surroundings without relying on sight, within a certain radius.",
+        reference: "PHB, pgs. 183-184.",
+        bullets: [
+            "Within a specified range, a creature with darkvision can see in darkness as if the darkness were dim light, so areas of darkness are only lightly obscured as far as that creature is concerned.",
+            "However, the creature can’t discern color in darkness, only shades of gray.",
+            "Many creatures in the worlds of D&D, especially those that dwell underground, have darkvision."
+        ]
+    },
+    {
+        title: "Truesight",
+        icon: "eye-shield",
+        subtitle: "See in darkness.",
+        description: "",
+        reference: "PHB, pg. 184.",
+        bullets: [
+            "A creature with truesight can, out to a specific range, see in normal and magical darkness, see invisible creatures and objects, automatically detect visual illusions and succeed on saving throws against them, and perceives the original form of a shapechanger or a creature that is transformed by magic.",
+            "Furthermore, the creature can see into the Ethereal Plane."
+        ]
+    },
+    {
+        title: "Half cover",
+        icon: "broken-shield",
+        subtitle: "",
+        description: "A target has half cover if an obstacle blocks at least half of its body.",
+        reference: "PHB, pg. 196.",
+        bullets: [
+            "The obstacle might be a low wall, a large piece of furniture, a narrow tree trunk, or a creature, whether that creature is an enemy or a friend.",
+            "A target with half cover has a +2 bonus to AC and Dexterity saving throws."
+        ]
+    },
+    {
+        title: "Three-quarters cover",
+        icon: "cracked-shield",
+        subtitle: "",
+        description: "A target has half cover if about three-quarters of it is covered by an obstacle.",
+        reference: "PHB, pg. 196.",
+        bullets: [
+            "The obstacle might be a portcullis, an arrow slit, or a thick tree trunk.",
+            "A target with three-quarters cover has a +5 bonus to AC and Dexterity saving throws."
+        ]
+    },
+    {
+        title: "Full cover",
+        icon: "checked-shield",
+        subtitle: "",
+        description: "target has total cover if it is completely concealed by an obstacle.",
+        reference: "PHB, pg. 196.",
+        bullets: [
+            "A target with total cover can’t be targeted directly by an attack or a spell, although some spells can reach such a target by including it in an area of effect."
+        ]
+    }
 ]
