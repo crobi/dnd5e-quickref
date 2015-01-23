c5059ddb6bfa0ff81e9e9bd581aee057168c6ea9
SecretOnline <stu.thom.96@gmail.com>
2015-01-23 15:43:32 +1300

    Set correct label for environment section

--- js/quickref.js
+++ js/quickref.js
@@ -62,7 +62,7 @@ function init() {
     fill_section(data_bonusaction, "basic-bonus-actions", "Bonus action");
     fill_section(data_reaction, "basic-reactions", "Reaction");
     fill_section(data_condition, "basic-conditions", "Condition");
-    fill_section(data_environment, "basic-environment", "Condition");
+    fill_section(data_environment, "basic-environment", "Environment");
 
     var modal = document.getElementById("modal");
     modal.onclick = hide_modal;
